07d3d26d979484947c0749c25fe8c3a475be739d
Felipe Franco <felipebuzzofranco@gmail.com>
2024-07-24 16:59:48 -0300

    feat: implement user signup functionality

--- src/app/components/login-screen/login-screen.component.ts
+++ src/app/components/login-screen/login-screen.component.ts
@@ -4,6 +4,7 @@ import { Component, OnInit, Inject, PLATFORM_ID } from '@angular/core';
 import { isPlatformBrowser } from '@angular/common';
 import { MessageService } from 'primeng/api';
 
+
 @Component({
   selector: 'app-login-screen',
   templateUrl: './login-screen.component.html',
@@ -16,12 +17,18 @@ export class LoginScreenComponent implements OnInit {
   toastIsVisible = false;
   isLoading = false;
   formularioLogin: FormGroup
+  formularioCadastro: FormGroup
 
   constructor(@Inject(PLATFORM_ID) private platformId: Object, private LoginService: LoginService, private messageService: MessageService) {
     this.formularioLogin = new FormGroup({
       email: new FormControl({ value: '', disabled: false }, [Validators.required, Validators.email]),
       password: new FormControl({ value: '', disabled: false }, [Validators.required, Validators.minLength(1)]),
-    });
+    })
+    this.formularioCadastro = new FormGroup({
+      name: new FormControl({ value: '', disabled: false }, [Validators.required]),
+      email: new FormControl({ value: '', disabled: false }, [Validators.required, Validators.email]),
+      password: new FormControl({ value: '', disabled: false }, [Validators.required, Validators.minLength(1)]),
+       });
   }
 
   ngOnInit() {
@@ -75,4 +82,31 @@ export class LoginScreenComponent implements OnInit {
       }
     );
   }
+
+  enviarRequisicaoDeCadastro() {
+    // Requisicao no service de cadastro
+    this.formularioCadastro.value.email = this.formularioCadastro.value.email.trim();
+    this.isLoading = true;
+    this.formularioCadastro.disable();
+    this.LoginService.signup(this.formularioCadastro.value.name, this.formularioCadastro.value.email, this.formularioCadastro.value.password).subscribe(
+      (success) => {
+        localStorage.setItem('token', `${(success as any)['token']}`);
+        localStorage.setItem('name', `${(success as any)['name']}`);
+        localStorage.setItem('id', `${(success as any)['id']}`);
+        this.messageService.add({ severity: 'success', summary: 'Cadastro efetuado com sucesso!', detail: 'Você será redirecionado à página em um instante...', sticky: false });
+        // Redirecionar para a página do usuário (a verificação de rule do token será feita posteriormente no servidor para a diferenciação de rotas)
+        this.isLoading = false;
+        setTimeout(() => {
+          window.location.href = '/player-page';
+        }, 3000);
+      },
+      (error: any) => {
+        // Se a requisição falhar, exibir mensagem de erro
+        this.messageService.add({ severity: 'error', summary: 'Erro ao efetuar cadastro', detail: 'Verifique suas credenciais e tente novamente.', sticky: false });
+        this.isLoading = false;
+        this.formularioCadastro.enable();
+      }
+    );
 }
+  
+  }
