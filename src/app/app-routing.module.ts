38788303130195c80f519d0c278914172ff70761
Felipe Franco <felipebuzzofranco@gmail.com>
2024-08-15 13:38:33 -0300

    feat: Add campaign page component and routing

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,6 +2,7 @@ import { LoginScreenComponent } from './components/login-screen/login-screen.com
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { SheetManagerComponent } from './components/sheet-manager/sheet-manager.component';
+import { CampaingPageComponent } from './components/campaing-page/campaing-page.component';
 
 const routes: Routes = [
   {
@@ -24,6 +25,10 @@ const routes: Routes = [
   {
     path: 'not-found',
     loadChildren: () => import('./components/page-not-found/page-not-found.module').then(m => m.PageNotFoundModule)
+  },
+  {
+    path: 'campaing-page',
+    loadChildren: () => import('./components/campaing-page/campaing-page.module').then(m => m.CampaingPageModule)
   },
   {
     path: '**',
