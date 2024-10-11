4c575317e508b7e6da997edf878fcb7f9f15149a
Henry Alexandre de Almeida Frungilo <henryfrungilo@outlook.com>
2024-10-11 09:17:15 -0300

    feat: create pages and components folder

---
+++ src/app/pages/login-page/login-page.component.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-login-page',
+  template: `
+    <div class="login-page">
+      <app-login-form></app-login-form>
+    </div>`,
+  styleUrls: ['./login-page.component.scss']
+})
+export class LoginPageComponent {
+
+}
---
+++ src/app/pages/login-page/login-page.module.ts
@@ -0,0 +1,13 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { LoginPageComponent } from './login-page.component';
+import { LoginPageRoutingModule } from './login-page-routing.module';
+
+@NgModule({
+  declarations: [LoginPageComponent],
+  imports: [
+    CommonModule,
+    LoginPageRoutingModule
+  ]
+})
+export class LoginPageModule { }
